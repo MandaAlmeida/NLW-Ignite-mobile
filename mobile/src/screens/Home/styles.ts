5b2da9bc5f2b34b30d5f6090ca07222b64a7ce5a
amanda almeida <97365951+amandaalmeida20@users.noreply.github.com>
2022-10-06 15:18:29 -0300

    Projeto final

---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 340,
+    height: 190,
+    marginTop: 150,
+    marginBottom: 48
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64
+  }
+});
